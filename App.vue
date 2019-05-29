4d962c1ff37a439e387f92e40576101c37b859e9
lijunyangliuxiangxiang <50786447+lijunyangliuxiangxiang@users.noreply.github.com>
2019-05-29 20:14:25 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <input type="text" v-model="text">
+    <button  @click="handadd">提交</button>
+    <ul >
+      <li 
+      v-for="(item, index) in list"
+      :key="index"
+      @click="removeItem(index)"
+      >
+       {{index+1}} .{{item}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return({
+        list: [
+         
+        ],
+        text: ''
+      })
+    },
+    methods: {
+      handadd() {
+        this.list.push(this.text)
+        this.text = ''
+      },
+      removeItem(index){
+        this.list.splice(index, 1)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
